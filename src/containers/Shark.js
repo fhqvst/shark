db571d65c1a8e03e67f4f5ba6c51612e690931fc
Filip Hallqvist <filiphallqvist@gmail.com>
2016-07-30 22:23:49 +0200

    Some styling

--- src/containers/Shark.js
+++ src/containers/Shark.js
@@ -6,17 +6,22 @@ const Shark = () => {
         <main>
             <div className="titlebar"></div>
             <header className="header">
-                <div className="search"></div>
+                <div className="search">
+                    <input type="search" className="search__input" placeholder="Search..." />
+                </div>
                 <div className="meta"></div>
             </header>
             <div className="content">
-                <div className="tabs">
-                    <div className="tabs__buttons">
-                        <button className="button tabs__button">Portfolio</button>
-                    </div>
-                    <div className="tabs__panes">
-                        <div className="tabs__pane">
-                            Portfölj
+                <div className="main">
+                    <div className="tabs">
+                        <div className="tabs__buttons">
+                            <button className="button tabs__button is-active">Portfolio</button>
+                            <button className="button tabs__button">Watchlist</button>
+                        </div>
+                        <div className="tabs__panes">
+                            <div className="tabs__pane">
+                                Portfölj
+                            </div>
                         </div>
                     </div>
                 </div>
